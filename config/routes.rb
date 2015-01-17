104b258b4e01d3f7b8d7f870f17354c421544d08
Simon Yi <yi.simon.k@gmail.com>
2015-01-16 20:15:44 -0500

    added first entry -instacart

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
   get "startups" => "pages#startups"
+  get "instacart" => "pages#let-instacart-be-your-grocery-sherpa"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
